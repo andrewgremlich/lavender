83cb61a5931bebab77437a81e6c8582b07eff3be
andrewgremlich <andrew.gremlich@gmail.com>
2026-04-07 16:27:14 -0600

    chore: update browser targets to 2-year support window (April 2024+)

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	build: {
-		target: ['chrome121', 'firefox122', 'safari17', 'edge121']
+		target: ['chrome124', 'firefox125', 'safari17.4', 'edge124']
 	},
 	define: {
 		'process.env.NODE_ENV': process.env.NODE_ENV === 'production' ? '"production"' : '"development"'
