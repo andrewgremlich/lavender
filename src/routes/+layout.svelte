a77a9cc4f9b7fb8b5b7052694a9027fe3a751631
andrewgremlich <andrew.gremlich@gmail.com>
2026-04-24 06:54:04 -0600

    styling reorder

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
+	import '../reset.css';
 	import "@fontsource/merriweather-sans"; // Defaults to weight 400
 	import "@fontsource/merriweather-sans/400.css"; // Specify weight
-	import "@fontsource/merriweather-sans/400-italic.css"; // Specify weight and style
-
 	import '../app.css';
-	import '../reset.css';
+
 	import { setupI18n } from '$lib/i18n';
 	import { waitLocale } from 'svelte-i18n';
 
